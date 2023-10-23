c0bae862762cc09a521030aeb61cfc647cdc28f2
Lorens <gueronlj@gmail.com>
2023-10-23 18:01:36 -0400

    read from file, split into groups of 3

--- src/Rucksack.java
+++ src/Rucksack.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Rucksack {
     public static ArrayList<Character> uniqueList = new ArrayList<Character>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         //SEPARATE THE DATA INTO ROWS   
         String data = AoCInputs.rucksack();
         String[] separated =  data.split("\r\n");
+    
+        //SPLIT EACH ROW IN HALF
         for (String singleRow : separated){    
-            //SPLIT THE ROW IN HALF
             int length = singleRow.length();
             char[] firstHalf = new char[length/2];
             singleRow.getChars(0, (length/2), firstHalf, 0);
@@ -18,16 +22,8 @@ public class Rucksack {
             singleRow.getChars((length/2), length, secondHalf, 0);
             findMatches(firstHalf, secondHalf);            
         }
-
-        calculateTotal();
-    }
-
-    private static void printArray(char[] array){
-        System.out.println("\r\n");
-        for (char c : array) {
-            System.out.print(c);
-        }
-        //System.out.println("\n");
+        //calculateTotal();
+        readFile();
     }
 
     private static void findMatches(char[] a, char[] b){
@@ -44,8 +40,7 @@ public class Rucksack {
                 matches.add(c);
             } 
         }
-        uniqueList.add(matches.get(0));
-        
+        uniqueList.add(matches.get(0));       
     }
 
     private static int calculateValue(String key){      
@@ -66,6 +61,7 @@ public class Rucksack {
         }
         System.out.println(total);
     }
+
     private static HashMap<String, Integer> valueMap(){
         //hashmap to store the priority of each character
         HashMap<String, Integer> priorities = new HashMap<String, Integer>();
@@ -77,7 +73,44 @@ public class Rucksack {
             value = value + 1;
             priorities.put(c.toUpperCase(), value + 25);  
         }
-
         return priorities;
     }
+
+    private static void readFile(){
+        //read text file
+        ArrayList<String[]> trios = new ArrayList<>();
+        BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("data.txt"));
+			String line;
+            int lineCount = 0;
+            ArrayList<String> trio = new ArrayList<String>();
+            //while there are lines to read...
+            while ((line = reader.readLine()) != null) {
+                lineCount++;
+                //Add the current line to the current group of 3
+                trio.add(line);
+                //If we've read 3 lines, add the group of 3 to the list of trios and clear the group of 3
+                if (lineCount % 3 == 0) {
+                    trios.add(trio.toArray(new String[0]));
+                    trio.clear();
+                } 
+            }
+            //If there are 1 or 2 lines left over, add them to the list of trios
+            if (lineCount % 3 != 0){
+                trios.add(trio.toArray(new String[0]));
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        //Print the trios
+        for (String[] array : trios) { 
+            for (String s : array) { 
+                System.out.println(s); 
+            }
+            //Print blank line in between trios
+            System.out.println(); 
+        }
+    }
 }
+
